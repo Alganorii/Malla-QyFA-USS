045caa40f36d0bff33f212ffe6d75c172e201077
Alganorii <tania.urrutia.mendoza3@gmail.com>
2025-07-27 04:04:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -86,8 +86,7 @@ const ramos = [
     "Microbiología Clínica y Parasitología",
     "Legislación Farmacéutica",
     "Formulación y Tecnología Farmacéutica II"
-  ]
-},
+] },
   { nombre: "Farmacia Asistencial", semestre: 8 },
 
   // 🌟 SEMESTRE 9
